02c5d1ea885a8299fa3a1b8759f982c068ef2ed2
Kyuu <lanvrc0@gmail.com>
2020-07-02 15:04:42 +0700

    Update bs-init.js

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	AOS.init({ disable: 'mobile' });
+	var isTouchDevice =
+    (('ontouchstart' in window) ||
+    (navigator.MaxTouchPoints > 0) ||
+    (navigator.msMaxTouchPoints > 0));
+			if(!isTouchDevice){
+	    console.log("lagi di non-touch");
+			AOS.init({disable: 'mobile'});
+			}else{
+	     console.log("lagi di touch");
+			 AOS.init({
+  disable: function() {
+    var maxWidth = 9000;
+    return window.innerWidth < maxWidth;
+  }
+});
+			}
 });

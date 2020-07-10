22b8d0a9d11720a72c3da2af61d38b43c39b4893
Kyuu <lanvrc0@gmail.com>
2020-07-10 19:10:25 +0700

    about me

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,18 +1,3 @@
 $(document).ready(function(){
-	var isTouchDevice =
-    (('ontouchstart' in window) ||
-    (navigator.MaxTouchPoints > 0) ||
-    (navigator.msMaxTouchPoints > 0));
-			if(!isTouchDevice){
-	    console.log("lagi di non-touch");
-			AOS.init({disable: 'mobile'});
-			}else{
-	     console.log("lagi di touch");
-			 AOS.init({
-  		 	disable: function() {
-    		var maxWidth = 9000;
-    		return window.innerWidth < maxWidth;
-  }
-});
-			}
+	AOS.init({ disable: 'mobile' });
 });
